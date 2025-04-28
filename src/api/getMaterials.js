33018bc1c13737e096d31ee49e6fe9a84b1e2331
Andrew-Bootsma <andrew_boots@proton.me>
2025-04-27 20:59:05 -0400

    getting there

--- src/api/getMaterials.js
+++ src/api/getMaterials.js
@@ -1,10 +1,7 @@
 import { API_BASE_URL } from "../config";
 
-export default async function getMaterials(page) {
-  const pageSize = 10;
-  const response = await fetch(
-    `${API_BASE_URL}/materials?_page=${page}&_per_page=${pageSize}`,
-  );
+export default async function getMaterials() {
+  const response = await fetch(`${API_BASE_URL}/materials`);
   const data = await response.json();
 
   return data;
